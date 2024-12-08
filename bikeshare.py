5ea2150ec47d00c390dd23bfc6869c973366bb62
David Barkah <dbarkah@ford.com>
2024-12-08 17:31:23 -0500

    Added list of user statistics

--- bikeshare.py
+++ bikeshare.py
@@ -162,7 +162,12 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """
+    Displays statistics on bikeshare users:
+    user type
+    gender and 
+    age
+    """
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
